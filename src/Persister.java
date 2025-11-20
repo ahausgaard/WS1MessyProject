2114841806f4c1626fe60d32683082a02f3ac3d6
Andreas Jakobsen <ahausgaardjakobsen@gmail.com>
2025-11-20 20:24:43 +0100

    Removed unnecessary comments and fixed naming in persister. Changed from public to private and fixed logic in addUser

--- src/Persister.java
+++ src/Persister.java
@@ -3,15 +3,15 @@ import java.util.*;
 
 public class Persister
 {
-    public static void s(ArrayList<User> u) throws IOException
+    public static void s(ArrayList<User> users) throws IOException
     {
-        File file = new File("users.txt"); // Creates file
-        FileWriter fw = new FileWriter(file); // Create filewriter
-        BufferedWriter bw = new BufferedWriter(fw); // Create BufferedWriter
-        for(int x=0; x<=u.size(); x++) // Create a loop
-        { // Nicely formatted block
-            bw.write(u.get(x).c_nam); // Write first thing
-            bw.write(u.get(x).getId()); // Write second thing
-        } // Ending parenthesis
+        File file = new File("users.txt");
+        FileWriter fileWriter = new FileWriter(file);
+        BufferedWriter bw = new BufferedWriter(fileWriter);
+        for(int x=0; x<=users.size(); x++)
+        {
+            bw.write(users.get(x).c_nam);
+            bw.write(users.get(x).getId());
+        }
     }
 }
--- src/UserList.java
+++ src/UserList.java
@@ -2,14 +2,13 @@ import java.util.ArrayList;
 
 public class UserList
 {
-    public ArrayList<User> users = new ArrayList<User>();
+    private ArrayList<User> users = new ArrayList<User>();
 
     public void addUser(User user)
     {
-        this.users.add(user);
         if(user == null)
         {
-            return;
+          this.users.add(user);
         }
     }
 
